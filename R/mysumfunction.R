5bc45e15653fe7cf5164aa2da6971ff467f6c153
DoraBoadi <dora.boadi@yahoo.com>
2024-09-12 12:14:35 +0000

    updated the mysumfunction

--- R/mysumfunction.R
+++ R/mysumfunction.R
@@ -14,3 +14,23 @@ mysumfunction <- function (s){
   s+1
 }
 
+
+
+#' Title
+#'
+#' @param s + a numeric value
+#'
+#' @return output  input number plus one
+#' @export
+#'
+#' @examples
+#' sumfunction_5(3)
+
+sumfunction_5 <- function (s){
+  s <- as.numeric(stringr::str_extract_all(s,"\\d+"))
+  if(is.character(s)){
+    s <- character(s)
+  }
+  s+5
+}
+
--- tests/testthat/test-mysumfunction.R
+++ tests/testthat/test-mysumfunction.R
@@ -1,4 +1,5 @@
 test_that("addition works", {
   expect_equal(mysumfunction(24),25)
   expect_equal(mysumfunction("teed7"), 8)
+  expect_equal(sumfunction_5(24),29)
 })
